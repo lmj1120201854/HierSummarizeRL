fec0c46afeb8c17ba7481041e8feeeb11e75e6d2
lmj1120201854 <3169670884@qq.com>
2026-04-02 00:44:06 +0800

    grpo distribution train

--- verl/scripts/run_grpo copy.sh
+++ verl/scripts/run_grpo copy.sh
@@ -1,4 +1,3 @@
-# 3机6卡(每个机器2卡npu)配置
 set -x
 
 ulimit -n 65535
@@ -12,16 +11,16 @@ export WANDB_ENTITY=lmj1120201854-beijing-institute-of-technology
 export WANDB_NAME=GRPO
 
 # verifier配置
-export COVER_VERIFIER_SERVER=10.0.2.85:8000
+export COVER_VERIFIER_SERVER=10.0.7.245:8000
 export COVER_VERIFIER_SERVER_NAME=HierSummarizeRL-Cover-Verifier
 
-export CF_VERIFIER_SERVER=10.0.1.70:8000
+export CF_VERIFIER_SERVER=10.0.2.183:8000
 export CF_VERIFIER_SERVER_NAME=HierSummarizeRL-CF-Verifier
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.prompt_key=prompt \
-    data.train_batch_size=24 \
+    data.train_batch_size=32 \
     data.max_prompt_length=4096 \
     data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
@@ -31,8 +30,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=/data/home/3120245632/scow/ai/appData/mjli/Models/Qwen3_8B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=24 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -40,22 +39,22 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
-    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.multi_turn.enable=False \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     reward_model.reward_manager=custom \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
     trainer.default_local_dir=/data/home/3120245632/scow/ai/appData/mjli/llm_proj/HierSummarizeRL/output/HierSummarizeRL-Qwen3-8B-RL \
-    trainer.n_gpus_per_node=2 \
-    trainer.nnodes=3 \
-    trainer.save_freq=100 \
+    trainer.n_gpus_per_node=4 \
+    trainer.nnodes=1 \
+    trainer.save_freq=10 \
     trainer.test_freq=-1 \
     trainer.val_before_train=False \
     trainer.resume_mode=disable \
@@ -69,5 +68,4 @@ python3 -m verl.trainer.main_ppo \
     trainer.experiment_name="$WANDB_NAME" \
     actor_rollout_ref.ref.use_torch_compile=False \
     actor_rollout_ref.actor.use_torch_compile=False \
-    ++data.filter_overlong_prompts_workers=8 \
     actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 $@
